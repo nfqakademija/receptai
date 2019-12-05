99f8d3a02aafe8cca1239b0e4ef2b6fe1942a13a
arnoldasjan <arnuske@yahoo.com>
2019-12-05 19:16:52 +0200

    Deleted Comments and added logger to image uploading

--- src/Controller/NewRecipeController.php
+++ src/Controller/NewRecipeController.php
@@ -10,6 +10,7 @@ use App\Form\IngredientType;
 use App\Form\NewRecipeType;
 use App\Form\TagType;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
@@ -27,62 +28,11 @@ class NewRecipeController extends AbstractController
     /**
      * @Route("/new/recipe", name="new_recipe")
      */
-    public function new(Request $request, EntityManagerInterface $entityManager)
+    public function new(Request $request, EntityManagerInterface $entityManager, LoggerInterface $logger)
     {
         $recipe = new Recipe();
 
         $form = $this->createForm(NewRecipeType::class);
-
-//        $form = $this->createFormBuilder()
-//            ->add('title', TextType::class, ['required' => true])
-//            ->add('description', TextareaType::class, ['required' => true])
-//            ->add('image', FileType::class, [
-//                'label' => 'Image',
-//
-//                // unmapped means that this field is not associated to any entity property
-//                'mapped' => false,
-//
-//                // make it optional so you don't have to re-upload the PDF file
-//                // everytime you edit the Product details
-//                'required' => false,
-//
-//                // unmapped fields can't define their validation using annotations
-//                // in the associated entity, so you can use the PHP constraint classes
-//                'constraints' => [
-//                    new File([
-//                        'maxSize' => '1024k',
-//                        'mimeTypes' => [
-//                            'image/jpeg',
-//                            'image/png',
-//                        ],
-//                        'mimeTypesMessage' => 'Please upload a valid image',
-//                    ])
-//                ],
-//            ])
-//            ->add('ingredients', CollectionType::class, [
-//                'entry_type' => IngredientType::class,
-//                'required' => true,
-//                'label' => false,
-//                'allow_add' => true,
-//                'allow_delete' => true,
-//                'by_reference' => false,
-//                'mapped' => false,
-//            ])
-//            ->add('tags', CollectionType::class, [
-//                'entry_type' => TagType::class,
-//                'required' => true,
-//                'label' => false,
-//                'allow_add' => true,
-//                'allow_delete' => true,
-//                'by_reference' => false,
-//            ])
-//            ->add('save', SubmitType::class, [
-//                'label' => 'Submit Recipe',
-//                'attr' => [
-//                    'class' => 'btn btn-success'
-//                ]
-//            ])
-//            ->getForm();
 
         $form->handleRequest($request);
 
@@ -111,7 +61,7 @@ class NewRecipeController extends AbstractController
                         $newFilename
                     );
                 } catch (FileException $e) {
-                    var_dump($e);
+                    $logger->critical('Image was not uploaded');
                 }
 
                 // updates the 'brochureFilename' property to store the PDF file name
