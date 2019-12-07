5d8db03c2d688e77ff79f5ab44566227f5747eee
Tautvydas Rusas <tautis63@gmail.com>
2019-12-07 12:04:32 +0200

    Vegan only tag works

--- src/Service/RecipesGenerator.php
+++ src/Service/RecipesGenerator.php
@@ -17,14 +17,32 @@ class RecipesGenerator
     public function getGeneratedRecipesId(ArrayCollection $selectedTags)
     {
         if (count($selectedTags) != 0) {
-            $generatedRecipeId = $this->recipeRepository->getNeededId($selectedTags);
+            $selectedTags = iterator_to_array($selectedTags);
 
-            if (count($generatedRecipeId) < 7) {
-                $remainingRecipeId = $this->recipeRepository->getRemainingRecipeId(
-                    $generatedRecipeId,
-                    7 - count($generatedRecipeId)
-                );
-                $generatedRecipeId = array_merge($generatedRecipeId, $remainingRecipeId);
+            if (in_array('Meat', $selectedTags) == false && in_array('Vegetarian', $selectedTags)) {
+                // Get only vegan recipes
+                if (count($selectedTags) == 1) {
+                    return $this->recipeRepository->getNeededVeganId();
+                } else {
+                    $selectedTags =  array_diff($selectedTags, (array)'Vegetarian');
+                    $generatedRecipeId = $this->recipeRepository->getNeededId($selectedTags);
+                    $remainingRecipeId = $this->recipeRepository->getRemainingVeganRecipeId(
+                        $generatedRecipeId,
+                        7 - count($generatedRecipeId)
+                    );
+                    $generatedRecipeId = array_merge($generatedRecipeId, $remainingRecipeId);
+                    return $generatedRecipeId;
+                }
+            } else {
+                $generatedRecipeId = $this->recipeRepository->getNeededId($selectedTags);
+
+                if (count($generatedRecipeId) < 7) {
+                    $remainingRecipeId = $this->recipeRepository->getRemainingRecipeId(
+                        $generatedRecipeId,
+                        7 - count($generatedRecipeId)
+                    );
+                    $generatedRecipeId = array_merge($generatedRecipeId, $remainingRecipeId);
+                }
             }
         } else {
             $remainingRecipeId =$this->recipeRepository->getRemainingRecipeId(array(0), 7);
