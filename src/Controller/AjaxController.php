a8351545fc6342a7dbd8b509dddfa93550fe781e
arentas <arentas.meinorius@gmail.com>
2019-12-13 01:56:42 +0200

    loads card in random space

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Tag;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Recipe;
@@ -20,11 +20,15 @@ class AjaxController extends AbstractController
         $recipe = $this->getDoctrine()->getRepository(Recipe::class)
             ->find($id);
         $tags = $recipe->getTags();
+        $tagObject = $this->getDoctrine()->getRepository(Tag::class)
+            ->find($tags[0]);
+        $recipes = $tagObject->getRecipes();
+        $random = random_int(0, sizeof($recipes) - 1);
 
         return $this->render('card/index.html.twig', [
-            'imageUrl' => "something",
-            'name' => "hi",
-            'id' => $id,
+            'imageUrl' => $recipes[$random]->getImageUrl(),
+            'name' => $recipes[$random]->getTitle(),
+            'id' => $recipes[$random]->getId(),
         ]);
     }
 }
