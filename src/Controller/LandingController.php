8bc2c90b87c9fd40b6631419f6aaa159a6ef458b
arentas <arentas.meinorius@gmail.com>
2019-12-15 01:45:37 +0200

    paths

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -8,12 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class LandingController extends AbstractController
 {
     /**
-     * @Route("/landing", name="landing")
+     * @Route("/", name="landing")
      */
     public function index()
     {
         return $this->render('landing/index.html.twig', [
-            'controller_name' => 'LandingController',
         ]);
     }
 }
